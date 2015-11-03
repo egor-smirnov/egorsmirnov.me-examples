266c1b8ed9ccd4686e82138b74625114b4f9f711
Egor Smirnov <egorsmir@gmail.com>
2015-11-03 21:33:23 +0100

    add example with babel 6 and babelify

---
+++ babel-5-to-6-babelify/gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+
+gulp.task('build', function () {
+    return browserify({entries: './app.jsx', extensions: ['.jsx'], debug: true})
+        .transform('babelify', {presets: ['es2015', 'react']})
+        .bundle()
+        .on('error', function (err) {
+            console.error(err);
+            this.emit('end');
+        })
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', ['build'], function () {
+    gulp.watch('*.jsx', ['build']);
+});
+
+gulp.task('default', ['watch']);
